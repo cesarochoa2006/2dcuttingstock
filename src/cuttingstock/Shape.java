88e9494d2997ecb78d4355620e2e3946f6137a25
DuncanvR <duncanvr@gmail.com>
2013-09-17 13:01:29 +0200

    Moved source files to cuttingstock package

--- src/cuttingstock/Shape.java
+++ src/cuttingstock/Shape.java
@@ -17,6 +17,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+package cuttingstock;
+
 public class Shape {
    protected final int    id, width, height;
    protected final String name;
